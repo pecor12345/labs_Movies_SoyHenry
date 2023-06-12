190e7e6f98b4c5ac48a41e6c471aaa57a6265751
Pedro Cornejo Mori <pe.cor.12345@gmail.com>
2023-06-12 17:40:37 -0500

    Ultima actualización

--- api_movies.py
+++ api_movies.py
@@ -1,166 +1,213 @@
-import pandas as pd
-import numpy as np
+from fastapi import FastAPI 
+import pandas as pd 
+import uvicorn 
 import os
-import ast
-from datetime import datetime
-from fastapi import FastAPI
-from movies import Movies
-from recommendation_system import Recommendation
-
-#                                           CARGA DE DATOS
+import sys
+import time
+import json
+import requests
+import urllib3
+import logging
+import argparse
+import traceback
+import re
+import random
+import string
+import datetime
+import threading
+import queue
+import concurrent.futures
+import urllib.parse
+import urllib.request
+import urllib.error
+#                                           MODULO API
+app = FastAPI()
 
 url='https://drive.google.com/file/d/1yqB8ahhhdiONfZLu8-xuU9qsWOgSf5Oo/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 df_labs = pd.read_csv(url)
 
-df_labs.info()
-df_labs.shape
-
-
-                            #DESARROLLO Y TRANSFORMACION DE DATOS
-#01.ELIMINAR COLUMNAS
-df_labs.drop(columns=['video', 'imdb_id', 'adult', 'original_title', 'poster_path', 'homepage', 'tagline'], axis=1, inplace = True)
-
-# 02.VALORES NULOS REVENUE Y BUDGET
-df_labs['budget'] = pd.to_numeric(df_labs['budget'], errors='coerce').fillna(0)
-df_labs['revenue'] = df_labs['revenue'].fillna(0)
-
-#03.Crear Columna Return
-df_labs['return'] = df_labs.apply(lambda x: x['revenue']/x['budget'] if x['budget'] != 0 else 0, axis=1)
-df_labs.info()
-
-#04. DESANIDAR 
-
-#04.1 String a Diccionario
-def str_to_dict(value):
-    if pd.isna(value):
-        return None
-    else:
-        return ast.literal_eval(value)
-#04.2 Desanidar Columnas
-df_labs['belongs_to_collection'] = df_labs['belongs_to_collection'].apply(str_to_dict)
-df_nested = pd.json_normalize(df_labs['belongs_to_collection']).fillna('')
-df_nested.head()
-
-columns = df_nested.columns
-new_columns_names = {column : f'belongs_to_collections_{column}' for column in columns}
-df_nested.rename(columns=new_columns_names, inplace=True)
-df_nested.head()
-df_nested.shape
-
-columns = ['genres', 'production_companies', 'production_countries', 'spoken_languages']
-
-for column in columns:
-    df_labs[column] = df_labs[column].apply(str_to_dict)
-    df_nested = pd.json_normalize(df_labs[column]).fillna('')
-    nested_columns = df_nested.columns
-    df_nested_1 = pd.DataFrame()
-    for nested_column in nested_columns:
-        df_aux = pd.json_normalize(df_nested[nested_column]).fillna('')
-        if 'production_countries' in column:
-            df_aux.rename(columns={'iso_3166_1': 'id'}, inplace=True)
-        if 'spoken_languages' in column:
-            df_aux.rename(columns={'iso_639_1': 'id'}, inplace=True)
-        df_aux.drop(columns='id', axis=1, inplace=True)
-        new_columns_names = {col : f'{column}_{nested_column}_{col}' for col in df_aux.columns}
-        df_aux.rename(columns=new_columns_names, inplace=True)
-        df_nested_1 = pd.concat([df_nested_1, df_aux], axis=1)
-
-# Convertir columns to a list of columns
-    column_name = f'{column}_name'
-    column_name_list = df_nested_1.columns.to_list()
-    df_nested_1[column_name] = df_nested_1.values.tolist()
-    df_nested_1.drop(columns=column_name_list, axis=1, inplace=True)
-    df_labs = pd.concat([df_labs, df_nested_1], axis=1)
-    df_labs.drop(columns=column, inplace=True)
-
-df_labs.shape
-
-#05. VALORES NULOS RELEASE_DATE
-df_labs.dropna(subset=['release_date'], inplace=True)
-df_labs.info()
-
-#06. FORMATO FECHA
-#06.1 CREAR COLUMNA RELEASE YEAR
-regex_filter = '^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$'
-filter = df_labs['release_date'].str.contains(regex_filter)
-df_labs = df_labs[filter]
-df_labs['release_year'] = df_labs['release_date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date().year)
-
-#06.2 CONVERTIR EL RELEASE DATE A FORMATO FECHA
-df_labs['release_date'] = pd.to_datetime(df_labs['release_date'], format='%Y-%m-%d')
-print(type(df_labs))
-
-#                                           MODULO API
-
-
-@app.get('/cantidad_filmaciones_dia/{mes}')
+# Función 01 : CANTIDAD DE FILMACIONES POR MES
+@app.get("/cantidad_filmaciones_mes/{mes}")
 async def cantidad_filmaciones_mes(mes: str):
-    df_combined['release_date'] = pd.to_datetime(df_combined['release_date'])
-    if mes.isdigit(): 
-
-    return {'Mes':mes, 'cantidad':filas}
-
-@app.get('/cantidad_filmaciones_dia/{dia}')
-def cantidad_filmaciones_dia(dia: str):
     df_labs['release_date'] = pd.to_datetime(df_labs['release_date'])
-    fecha_filtrada = pd.to_datetime(dia)  # Fecha específica a filtrar
-    df_filtrado = df_labs[df_labs['release_date'] == fecha_filtrada]
-    filas = df_filtrado.loc[:]
+    if mes.isdigit():
+        mes_num = int(mes)
+        cantidad = df_labs[df_labs['release_date'].dt.month == mes_num].shape[0]
+    else:
+        mes = mes.lower()
+        meses_nombres = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+                         'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+        if mes in meses_nombres:
+            mes_num = meses_nombres.index(mes) + 1
+            cantidad = df_labs[df_labs['release_date'].dt.month == mes_num].shape[0]
+        else:
+            return {"Error. No hay información. Revise el mes ingresado por favor."}
+    return {"En el mes de ": mes, " fueron estrenadas la siguiente cantidad de peliculas:": cantidad}
 
-    return {'dia':dia, 'cantidad':filas}
+#Función 02: CANTIDAD DE FILMACIONES POR DÍA
+@app.get("/cantidad_filmaciones_dia/{dia}")
+async def cantidad_filmaciones_dia(dia: str):
+    if dia.isdigit():
+        dia_num = int(dia)
+    else:
+        dia = dia.lower()
+        dias_nombres = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+        if dia in dias_nombres:
+            dia_num = dias_nombres.index(dia) + 1
+        else:
+            return {"Error. El día ingresado no es válido. Ingrese nuevamente el día."}
+        
+    cantidad = df_labs[df_labs['release_date'].dt.day == dia].shape[0]
+    cantidad = df_labs[df_labs['release_date'].dt.day == dia_num].shape[0]
 
+    return {"En el dia ": dia, "se estrenaron la siguiente cantidad de películas:": cantidad}
 
-@app.get('/score_titulo/{titulo}')
-def score_titulo(titulo:str):
-    '''Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score'''
-    titulo_filtrado = df_labs[df_labs['title'] ==t
+#Función 03: PELICULA DONDE SE INCLUYE AÑO DE ESTRENO Y PUNTAJE
+@app.get("/titulo_de_la_filmacion/{titulo}")
+async def titulo_de_la_filmacion(titulo: str):
+    titulo = titulo.lower()
+    peliculas = df_labs[df_labs['title'].str.lower().str.contains(titulo)]
+    if peliculas.shape[0] == 0:
+        return {"Error. No hay informacion para esta película, por favor revise el Titulo e intentelo nuevamente."}
+    else:
+        pelicula = peliculas.iloc[0]
+    
+        devolucion = {
+        "Pelicula": peliculas.iloc[0]['title'], 
+        "Año de estreno": str(peliculas.iloc[0]['release_year']),
+        "Valoración": str(peliculas.iloc[0]['vote_average'])
+        }
+    return devolucion
 
-
-films = [
-    {"titulo": "Pelicula 1", "anio_estreno": 2020, "score": 8.5},
-    {"titulo": "Pelicula 2", "anio_estreno": 2019, "score": 7.8},
-    {"titulo": "Pelicula 3", "anio_estreno": 2021, "score": 9.2},
-    # Agrega más películas aquí
-]
-
-@app.get("/peliculas/{titulo}")
-def obtener_pelicula(titulo: str):
-    for pelicula in films:
-        if pelicula["titulo"] == titulo:
-            return {
-                "titulo": pelicula["titulo"],
-                "anio_estreno": pelicula["anio_estreno"],
-                "score": pelicula["score"]
+#Función 04: PELICULA DONDE SE INCLUYE NOMBRE, CANTIDAD DE VOTOS Y PROMEDIO DE VALORACIÓN
+@app.get("/votos_titulo/{votos}")
+async def votos_titulo(votos: str):
+    titulo = titulo.lower()
+    peliculas = df_labs[df_labs['title'].str.lower() == titulo]
+    if peliculas.empty:
+        return {"Error, Titulo no cuenta con la información solicitada"}
+    else:
+        pelicula = peliculas.iloc[0]
+        if pelicula['vote_count'] < 2000:
+            return {f"Error. La película {titulo} no tiene suficientes votos"}
+        else:
+            devolucion = {
+                "titulo": str(pelicula['title']),
+                "anio": str(pelicula['release_year']),
+                "voto_total": str(pelicula['vote_count']),
+                "voto_promedio": str(pelicula['vote_average'])
             }
-    return {"mensaje": "Pelicula no encontrada"}
+            return devolucion
+
+#Función 05: ACTOR Y SUS PARTICIPACIONES
+@app.get("/nombre_actor/{actor}")
+async def nombre_actor(actor: str):
+    actor = actor.lower()
+    actores = df_labs[df_labs['cast'].str.lower().str.contains(actor)]
+
+    if actores.shape[0] == 0:
+        return {"Error. No hay informacion para este actor, por favor revise el nombre e intentelo nuevamente."}
+    else:
+        actor = actores.iloc[0]
+        cantidad_peliculas = actores.shape[0]
+        retorno_promedio = actores / cantidad_peliculas if cantidad_peliculas > 0 else 0
+
+        devolucion = {
+            "Actor : ": actor['cast'], 
+            "Participaciones: ": actor['num_critic_for_reviews'],
+            "Promedio participaciones: ": retorno_promedio
+        }
+
+        return devolucion
+   
+ #Función 06: DIRECTOR Y SUS EXITOS
+@app.get("/nombre_director/{director}")
+async def nombre_director(director: str):
+    director = director.lower()
+    df_labs['crew'] = df_labs['crew'].str.lower()
+    peliculas_del_director = df_labs[df_labs['crew'].str.contains(director, na=False)]
+
+    if peliculas_del_director.shape[0] == 0:
+        return {"Error. Director no encontrado"}
+    else:
+        resultados = []
+        for _, pelicula in peliculas_del_directo.iterrows():
+            resultados.append({
+                "titulo": str(pelicula['title']),
+                "anio": str(pelicula['release_year']),
+                "retorno_pelicula": str(pelicula['return']),
+                "budget_pelicula": str(pelicula['budget']),
+                "revenue_pelicula": str(pelicula['revenue'])
+            })
+        retorno_total = peliculas_del_director['return'].sum()
+
+        return {
+            "director": director,
+            "retorno_total_director": str(retorno_total),
+            "peliculas": resultados
+            }
+    
+#importar funciones
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer 
+
+#Limpiar datos, crear caracteristicas y combinar colunmas
+df_labs['genres'] = df_labs['genres'].fillna('')
+df_labs['features'] = df_labs['genres'].astype(str) + ' ' + df_labs['vote_average'].astype(str)
+
+# Crear a TF-IDF matrix para caracteristicas combinadas
+tfidf = TfidfVectorizer()
+tfidf_matrix = tfidf.fit_transform(df_combined['features'])
+
+#Definiendo la función de Recomendación
+def recomendacion(titulo):
+    titulo = titulo.lower()
+    pelicula_seleccionada = df_labs[df_labs['title'].str.lower() == titulo]
+
+    if pelicula_seleccionada.empty:
+        return []
+
+ #Filtrar peliculas por titulo   
+    index = pelicula_seleccionada.index[0]
+#Obtener el ID de la pelicula seleccionada
+    id_pelicula_seleccionada = pelicula_seleccionada.iloc
+# calcular la similitud entre peliculas 
+    similarity_scores = cosine_similarity(tfidf_matrix[index], tfidf_matrix)
+# Ranking de peliculas parecidas
+    similar_movies_indices = similarity_scores.argsort()[0][::-1]
+    similar_movies_indices = similar_movies_indices[similar_movies_indices != index]
+    peliculas_similares = df_combined.iloc[similar_movies_indices][:5][['title', 'vote_average']].values.tolist()
+#Peliculas con alta valoración
+    peliculas_similares = sorted(peliculas_similares, key=lambda x: x[1], reverse=True)
+    while len(peliculas_similares) < 5:
+        peliculas_similares.append(['Película adicional', 0])
+
+    return peliculas_similares
+
+ #Crear una función para recomendar películas
+@app.get("/recomendacion/{titulo}")
+async def obtener_recomendacion(titulo: str):
+    recomendaciones = recomendacion(titulo)
+    if len(recomendaciones) == 0:
+        return {"Error": "Película no encontrada"}
+    else:
+        return {"lista_recomendada": recomendaciones}
 
 
-    return {'titulo':titulo, 'anio':respuesta, 'popularidad':respuesta}
 
-@app.get('/votos_titulo/{titulo}')
-def votos_titulo(titulo:str):
-    '''Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor promedio de las votaciones. 
-    La misma variable deberá de contar con al menos 2000 valoraciones, 
-    caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, no se devuelve ningun valor.'''
-    return {'titulo':titulo, 'anio':respuesta, 'voto_total':respuesta, 'voto_promedio':respuesta}
+    
 
-@app.get('/get_actor/{nombre_actor}')
-def get_actor(nombre_actor:str):
-    '''Se ingresa el nombre de un actor que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
-    Además, la cantidad de películas que en las que ha participado y el promedio de retorno'''
-    return {'actor':nombre_actor, 'cantidad_filmaciones':respuesta, 'retorno_total':respuesta, 'retorno_promedio':respuesta}
 
-@app.get('/get_director/{nombre_director}')
-def get_director(nombre_director:str):
-    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
-    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.'''
-    return {'director':nombre_director, 'retorno_total_director':respuesta, 
-    'peliculas':respuesta, 'anio':respuesta,, 'retorno_pelicula':respuesta, 
-    'budget_pelicula':respuesta, 'revenue_pelicula':respuesta}
+                         
+                         
+           
+                      
+        
 
-# ML
-@app.get('/recomendacion/{titulo}')
-def recomendacion(titulo:str):
-    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-    return {'lista recomendada': respuesta}
+            
+                        
+
+        
+                
+    
